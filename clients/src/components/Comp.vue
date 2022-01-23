5e6d9fe910627e19e8254fd42b6bb428964d6845
NgoQuocBao1010 <56999031+NgoQuocBao1010@users.noreply.github.com>
2022-01-23 22:19:08 +0700

    ⚙ conf: new branch test

---
+++ clients/src/components/Comp.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    props: {
+        message: {
+            type: String,
+        },
+    },
+};
+</script>
+
+<template>
+    <p>New {{ message }}</p>
+</template>
+
+<style lang="scss" scoped></style>
