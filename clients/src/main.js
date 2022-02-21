5e9a39c83fbf49cb16164abb0535bbd578c2a3f9
Quoc Bao <totti.chelsea@gmail.com>
2022-02-21 17:35:32 +0700

    ⚙️ conf: toast, popup confirm configuration

--- clients/src/main.js
+++ clients/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
+import ConfirmationService from "primeVue/confirmationservice";
+import ToastService from "primeVue/toastservice";
 import "primevue/resources/themes/lara-dark-blue/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
@@ -12,6 +14,8 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(PrimeVue, { ripple: true });
+app.use(ConfirmationService);
+app.use(ToastService);
 app.use(router);
 
 app.mount("#app");
