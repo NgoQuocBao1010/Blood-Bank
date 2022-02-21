62cfa07e2211481ed893212621f1151b99e4db2c
NgoQuocBao1010 <totti.chelsea@gmail.com>
2022-02-21 11:34:53 +0700

    ⚙️ conf: load Primevue library into Vue app

--- clients/src/main.js
+++ clients/src/main.js
@@ -1,12 +1,17 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/lara-dark-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(PrimeVue, { ripple: true });
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
