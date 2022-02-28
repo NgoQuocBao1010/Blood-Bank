4eee1cec5687e5a802e478d67d1a4de3b7e75cb3
Quoc Bao <totti.chelsea@gmail.com>
2022-02-28 17:58:00 +0700

    🆕 feat: sidebar layout

---
+++ clients/src/layouts/AppSidebar.vue
@@ -0,0 +1,39 @@
+<script setup>
+const menu = [
+    {
+        label: "Home",
+        items: [
+            {
+                label: "Dashboard",
+                icon: "pi pi-fw pi-home",
+                to: "/",
+            },
+        ],
+    },
+    {
+        label: "Get Started",
+        items: [
+            {
+                label: "Documentation",
+                icon: "pi pi-fw pi-question",
+                command: () => {
+                    window.location = "#/documentation";
+                },
+            },
+            {
+                label: "View Source",
+                icon: "pi pi-fw pi-search",
+                command: () => {
+                    window.location = "https://github.com/primefaces/sakai-vue";
+                },
+            },
+        ],
+    },
+];
+</script>
+
+<template>
+    <p>Sidebar</p>
+</template>
+
+<style lang="scss" scoped></style>
