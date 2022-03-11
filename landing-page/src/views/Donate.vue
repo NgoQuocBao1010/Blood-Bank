cc59e376625230cf63960cc859c64423266fa827
lcnhut070200 <lcnhut070200@gmail.com>
2022-03-11 23:03:46 +0700

    Setup landing-page

---
+++ landing-page/src/views/Donate.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>Donate Page</h1>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ landing-page/src/views/Welcome/Welcome.vue
@@ -0,0 +1,30 @@
+<script setup>
+import EventSlider from "./EventSlider.vue";
+import AboutDonationSection from "./AboutDonationSection.vue";
+import DonationTypesSection from "./DonationTypesSection.vue";
+</script>
+
+<template>
+  <div class="welcome">
+    <div class="section slider-section">
+      <EventSlider />
+    </div>
+    <div class="section about_donation-section">
+      <AboutDonationSection />
+    </div>
+
+    <div class="section donation_type-section">
+      <DonationTypesSection />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.welcome {
+  .section {
+    margin-bottom: 20px;
+    border-bottom: 1px solid #ccc;
+    padding-bottom: 20px;
+  }
+}
+</style>
