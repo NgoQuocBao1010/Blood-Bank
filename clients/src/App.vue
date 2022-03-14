279edeb04473e2f19e0ac935414d9470e416e1c5
Quoc Bao <totti.chelsea@gmail.com>
2022-03-14 20:03:27 +0700

    🆕 feat: override primevue textinput css

--- clients/src/App.vue
+++ clients/src/App.vue
@@ -8,45 +8,37 @@ import Footer from "./components/Footer.vue";
 let sidebarHide = $ref(null);
 
 const winWidth =
-  window.innerWidth ||
-  document.documentElement.clientWidth ||
-  document.body.clientWidth;
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
 
 onMounted(() => {
-  // Hide sidebar if window's width is lower than 1280
-  sidebarHide = winWidth <= 1280 ? true : false;
+    // Hide sidebar if window's width is lower than 1280
+    sidebarHide = winWidth <= 1280 ? true : false;
 });
 </script>
 
 <template>
-  <main
-    class="layout-wrapper layout-static"
-    :class="{ 'layout-static-sidebar-inactive': sidebarHide }"
-  >
-    <!-- Navbar -->
-    <AppNavbar @toggleSidebar="sidebarHide = !sidebarHide" />
+    <main
+        class="layout-wrapper layout-static"
+        :class="{ 'layout-static-sidebar-inactive': sidebarHide }"
+    >
+        <!-- Navbar -->
+        <AppNavbar @toggleSidebar="sidebarHide = !sidebarHide" />
 
-    <!-- Sidebar -->
-    <div class="layout-sidebar">
-      <AppSidebar />
-    </div>
+        <!-- Sidebar -->
+        <div class="layout-sidebar">
+            <AppSidebar />
+        </div>
 
-    <!-- Main -->
-    <div class="layout-main-container">
-      <div class="layout-main">
-        <router-view />
-      </div>
-      <Footer />
-    </div>
-  </main>
+        <!-- Main -->
+        <div class="layout-main-container">
+            <div class="layout-main">
+                <router-view />
+            </div>
+            <Footer />
+        </div>
+    </main>
 </template>
 
-<style lang="scss">
-.my-btn {
-  margin: 1rem 0;
-  width: max-content;
-  &:focus {
-    box-shadow: none !important;
-  }
-}
-</style>
+<style lang="scss"></style>
