aa9c7954a2caa37b1f4e4c9f1491b0c57598d767
truonghoangthuan <tthun32@gmail.com>
2022-03-19 16:12:47 +0700

    add /api/ to the routes of UserController.cs HospitalController.cs RequestController.cs

--- backend/Controllers/RequestController.cs
+++ backend/Controllers/RequestController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace backend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class RequestController : ControllerBase
     {
         private readonly IRequestRepository _requestRepository;
--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace backend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
         private readonly IUserRepository _userRepository;
