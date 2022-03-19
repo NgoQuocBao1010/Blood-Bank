0e6b7b4d5cd131e2c460ff7016f2b2732df8f6a8
truonghoangthuan <tthun32@gmail.com>
2022-03-19 14:28:12 +0700

    add return 404 error for GET RequestController.cs, UserController.cs. add delete for RequestController.cs

--- backend/Controllers/RequestController.cs
+++ backend/Controllers/RequestController.cs
@@ -22,11 +22,16 @@ namespace backend.Controllers
             var result = await _requestRepository.Create(request);
             return new JsonResult(result);
         }
-        
+
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
         {
             var result = await _requestRepository.Get(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
             return new JsonResult(result);
         }
 
@@ -34,6 +39,11 @@ namespace backend.Controllers
         public async Task<IActionResult> Get()
         {
             var result = await _requestRepository.Get();
+            if (result == null)
+            {
+                return NotFound();
+            }
+
             return new JsonResult(result);
         }
 
@@ -41,6 +51,13 @@ namespace backend.Controllers
         public async Task<IActionResult> Update(string id, Request request)
         {
             var result = await _requestRepository.Update(id, request);
+            return new JsonResult(result);
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var result = await _requestRepository.Delete(id);
             return new JsonResult(result);
         }
     }
--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -42,6 +42,11 @@ namespace backend.Controllers
         public async Task<IActionResult> Get(string id)
         {
             var user = await _userRepository.Get(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            
             return new JsonResult(user);
         }
 
@@ -49,6 +54,11 @@ namespace backend.Controllers
         public async Task<IActionResult> Get()
         {
             var user = await _userRepository.Get();
+            if (user == null)
+            {
+                return NotFound();
+            }
+            
             return new JsonResult(user);
         }
 
