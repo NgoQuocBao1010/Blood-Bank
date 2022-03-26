acb40958e86ce3db26f4e5371854e5c69ea4563a
lcnhut070200 <lcnhut070200@gmail.com>
2022-03-26 21:07:44 +0700

    style: remove forgot password

--- admin-page/src/views/Login.vue
+++ admin-page/src/views/Login.vue
@@ -128,9 +128,6 @@ const submitData = async () => {
                 ></Checkbox>
                 <label for="rememberme1">Remember me</label>
               </div>
-              <a class="font-medium no-underline ml-2 text-right cursor-pointer"
-                >Forgot password?</a
-              >
             </div>
             <Button
               label="Sign In"
