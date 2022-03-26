91d03cfb08dffbbada94d8c8231eb96fb4f157e7
Quoc Bao <totti.chelsea@gmail.com>
2022-03-26 14:14:06 +0700

    🆕 feat: login API call

--- admin-page/src/api/UserRepo.js
+++ admin-page/src/api/UserRepo.js
@@ -4,6 +4,6 @@ const resource = "/user";
 
 export default {
     getToken(email, password) {
-        return Repository.get(`${resource}/login/`);
+        return Repository.post(`${resource}/login/`, { email, password });
     },
 };
--- admin-page/src/stores/user.js
+++ admin-page/src/stores/user.js
@@ -29,15 +29,12 @@ export const useUserStore = defineStore("user", {
             return this.token ? false : true;
         },
         async login(email, password) {
-            try {
-                const response = await UserRepo.getToken(email, password);
-                console.log(response);
+            const { data } = await UserRepo.getToken(email, password);
 
-                const authToken = "45808de6-ce77-436e-b698-c64d8f3148ae";
-                this.setToken(authToken);
-                this.email = email;
-                this.isLoggedIn = true;
-            } catch (e) {}
+            const authToken = data.token;
+            this.setToken(authToken);
+            this.email = email;
+            this.isLoggedIn = true;
         },
         logout() {
             localStorage.removeItem("userToken");
