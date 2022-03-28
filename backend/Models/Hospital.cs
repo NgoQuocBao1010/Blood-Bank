3c27eef69bfec08af320eeb18ff311a671e49f03
trongbui1105 <buiquoctrong110500@gmail.com>
2022-03-28 13:57:49 +0700

    Update Hospital

--- backend/Models/Hospital.cs
+++ backend/Models/Hospital.cs
@@ -5,19 +5,18 @@ namespace backend.Models
 {
     public class Hospital
     {
-        public Hospital(string hospital_name, string address, string phone)
-        {
-            this.hospital_name = hospital_name;
-            this.address = address;
-            this.phone = phone;
-        }
-        
         [BsonRepresentation(BsonType.ObjectId)]
         public string _id { get; set; }
         
-        public string hospital_name { get; set; }
-        public string address { get; set; }
-        public string phone { get; set; }
-        
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public Hospital(string name, string address, string phone)
+        {
+            Name = name;
+            Address = address;
+            Phone = phone;
+        }
     }
 }
