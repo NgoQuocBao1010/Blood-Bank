34ed2ff4b32baee171508647220591a716950f09
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-03-29 11:03:41 +0700

    🆕 feat: donor detail - get API, donations of 1 donor

--- admin-page/src/api/DonorTransaction.js
+++ admin-page/src/api/DonorTransaction.js
@@ -3,6 +3,9 @@ import Repository from "./Repository";
 const resource = "/donortransaction";
 
 export default {
+    getListTransactionByDonor(donorID) {
+        return Repository.get(`${resource}/listDonation/${donorID}`);
+    },
     approveParticipants(data) {
         return Repository.put(`${resource}/approve`, data);
     },
