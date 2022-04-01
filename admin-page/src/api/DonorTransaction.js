603a6dbfb12ea9f2dc76a62fe6319ba13b8d521d
trongbui1105 <buiquoctrong110500@gmail.com>
2022-04-01 20:40:07 +0700

    Fix Authorize

--- admin-page/src/api/DonorTransaction.js
+++ admin-page/src/api/DonorTransaction.js
@@ -4,12 +4,15 @@ const resource = "/donortransaction";
 
 export default {
     getListTransactionByDonor(donorID) {
+        useLocalToken();
         return Repository.get(`${resource}/listDonation/${donorID}`);
     },
     approveParticipants(data) {
+        useLocalToken();
         return Repository.put(`${resource}/approve`, data);
     },
     rejectParticipants(data) {
+        useLocalToken();
         return Repository.put(`${resource}/reject`, data);
     },
 };
