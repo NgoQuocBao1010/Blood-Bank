b1c8db433c65291dc0383eba1bae033c5e901cd3
trongbui1105 <buiquoctrong110500@gmail.com>
2022-04-05 18:39:01 +0700

    API GET general information of Dashboard

--- backend/Controllers/DashboardController.cs
+++ backend/Controllers/DashboardController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.Models;
@@ -17,13 +18,15 @@ namespace backend.Controllers
         private readonly IDonorRepository _donorRepository;
         private readonly IDonorTransactionRepository _donorTransactionRepository;
         private readonly IRequestRepository _requestRepository;
+        private readonly IEventRepository _eventRepository;
 
         public DashboardController(IDonorRepository donorRepository, IRequestRepository requestRepository,
-            IDonorTransactionRepository donorTransactionRepository)
+            IDonorTransactionRepository donorTransactionRepository, IEventRepository eventRepository)
         {
             _donorRepository = donorRepository;
             _requestRepository = requestRepository;
             _donorTransactionRepository = donorTransactionRepository;
+            _eventRepository = eventRepository;
         }
 
         [HttpGet]
@@ -33,10 +36,11 @@ namespace backend.Controllers
             var result = new List<RecentActivities>();
             var listApprovedTransactions = await _donorTransactionRepository.GetTransactionByStatus(1);
             var listId = listApprovedTransactions
-                .Select(transaction => new Id(transaction.donorId, transaction.dateDonated, "transaction")).ToList();
+                .Select(transaction =>
+                    new Id(transaction.donorId, transaction.dateDonated, "transaction", transaction._id)).ToList();
 
             var listApprovedRequest = await _requestRepository.GetRequestByStatus(1);
-            listId.AddRange(listApprovedRequest.Select(request => new Id(request._id, request.Date, "request")));
+            listId.AddRange(listApprovedRequest.Select(request => new Id(request._id, request.Date, "request", null)));
 
             listId = new List<Id>(listId.OrderByDescending(id => long.Parse(id.date)));
 
@@ -48,12 +52,12 @@ namespace backend.Controllers
                     case "transaction":
                         var donor = await _donorRepository.Get(id._id);
                         recentActivity = new RecentActivities(id._id, "Blood Receive", donor.name,
-                            id.date);
+                            id.date, id.transactionId);
                         break;
                     case "request":
                         var request = await _requestRepository.Get(id._id);
                         recentActivity = new RecentActivities(id._id, "Blood Donated", request.HospitalName,
-                            id.date);
+                            id.date, null);
                         break;
                 }
 
@@ -71,38 +75,226 @@ namespace backend.Controllers
         [Route("dashboardInfo")]
         public async Task<IActionResult> GetDashboardInfo()
         {
-            var currentTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-            var result = await GetBloodReceiveLastQuarter(currentTime);
+            // get current time
+            var currentUnixTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            // convert to current date
+            var currentDate = DateTimeOffset.FromUnixTimeMilliseconds(currentUnixTime).DateTime;
+            // get an index of quarter
+            var quarterNumber = (currentDate.Month - 1) / 3 + 1;
+            // get first day of current quarter
+            DateTimeOffset firstDayOfCurrentQuarter = new DateTime(currentDate.Year, (quarterNumber - 1) * 3 + 1, 1);
+            // get last day of current quarter
+            var lastDayOfCurrentQuarter = firstDayOfCurrentQuarter.AddMonths(3).AddDays(-1);
+            // get first day of last quarter
+            var firstDayOfLastQuarter = firstDayOfCurrentQuarter.AddMonths(-3);
+            if (quarterNumber == 1)
+            {
+                firstDayOfLastQuarter = firstDayOfCurrentQuarter.AddYears(-1).AddMonths(-3);
+            }
 
+            // get last day of last quarter
+            var lastDayOfLastQuarter = firstDayOfLastQuarter.AddMonths(3).AddDays(-1);
+
+            // get total and percentage of blood receive when compare to last quarter
             var totalBloodReceive = await GetTotalBloodReceive();
-            return new JsonResult(result);
+            var bloodReceiveLastQuarter = await GetBloodReceiveLastQuarter(
+                firstDayOfLastQuarter.ToUnixTimeMilliseconds(),
+                lastDayOfLastQuarter.ToUnixTimeMilliseconds(), firstDayOfCurrentQuarter.ToUnixTimeMilliseconds());
+            var bloodReceive = new BloodReceive(totalBloodReceive, bloodReceiveLastQuarter);
+
+            // get total and percentage of blood donated when compare to last quarter
+            var totalBloodDonated = await GetTotalBloodDonated();
+            var bloodDonatedLastQuarter = await GetBloodDonatedLastQuarter(
+                firstDayOfLastQuarter.ToUnixTimeMilliseconds(),
+                lastDayOfLastQuarter.ToUnixTimeMilliseconds(), firstDayOfCurrentQuarter.ToUnixTimeMilliseconds());
+            var bloodDonated = new BloodDonated(totalBloodDonated, bloodDonatedLastQuarter);
+
+            // get total and percentage of donators when compare to last quarter
+            var totalDonators = await GetTotalDonators();
+            var donatorsLastQuarter = await GetDonatorsLastQuarter(
+                firstDayOfLastQuarter.ToUnixTimeMilliseconds(),
+                lastDayOfLastQuarter.ToUnixTimeMilliseconds(), firstDayOfCurrentQuarter.ToUnixTimeMilliseconds());
+            var donators = new Donators(totalDonators.Count, donatorsLastQuarter);
+
+            // get total and percentage of events when compare to last quarter
+            var totalEvents = await GetTotalEvents();
+            var eventsLastQuarter = await GetEventsLastQuarter(
+                firstDayOfLastQuarter.ToUnixTimeMilliseconds(),
+                lastDayOfLastQuarter.ToUnixTimeMilliseconds(), firstDayOfCurrentQuarter.ToUnixTimeMilliseconds(),
+                lastDayOfCurrentQuarter.ToUnixTimeMilliseconds());
+            var events = new Events(totalEvents, eventsLastQuarter);
+
+            var dashboardInfo = new DashboardInfo(bloodReceive, bloodDonated, donators, events);
+
+            return new JsonResult(dashboardInfo);
         }
 
-        public async Task<double> GetTotalBloodReceive()
+        public async Task<float> GetTotalBloodReceive()
         {
             var transactions = await _donorTransactionRepository.GetTransactionByStatus(1);
-            double total = transactions.Sum(transaction => transaction.amount);
+            float total = transactions.Sum(transaction => transaction.amount);
             return total;
         }
 
-        public async Task<double> GetBloodReceiveLastQuarter(long currentTime)
+        public async Task<float> GetBloodReceiveLastQuarter(long firstDayOfLastQuarter, long lastDayOfLastQuarter,
+            long firstDayOfCurrentQuarter)
         {
-            var currentDate = DateTimeOffset.FromUnixTimeMilliseconds(currentTime).DateTime;
-            DateTimeOffset lastQuarter = currentDate.AddMonths(-3);
-            var lastUnixQuarter = lastQuarter.ToUnixTimeMilliseconds();
-
             var transactions = await _donorTransactionRepository.GetTransactionByStatus(1);
-            var bloodReceiveLastQuarter = transactions.Where(transaction => long.Parse(transaction.dateDonated) >= lastUnixQuarter)
-                .Aggregate<DonorTransaction, double>(0, (current, transaction) => current + transaction.amount);
+            float bloodReceiveLastQuarter = 0;
+            float bloodReceiveCurrentQuarter = 0;
 
-            var totalBloodReceive = await GetTotalBloodReceive();
-            if (Math.Abs(totalBloodReceive - bloodReceiveLastQuarter) == 0)
+            foreach (var transaction in transactions)
+            {
+                var date = long.Parse(transaction.dateDonated);
+                if (date >= firstDayOfLastQuarter && date <= lastDayOfLastQuarter)
+                {
+                    bloodReceiveLastQuarter += transaction.amount;
+                }
+
+                if (date >= firstDayOfCurrentQuarter)
+                {
+                    bloodReceiveCurrentQuarter += transaction.amount;
+                }
+            }
+
+            if (Math.Abs(bloodReceiveLastQuarter - bloodReceiveCurrentQuarter) == 0)
             {
                 return 0;
             }
-            var percent = totalBloodReceive / (totalBloodReceive - bloodReceiveLastQuarter);
-            
-            return totalBloodReceive - bloodReceiveLastQuarter;
+
+            if (bloodReceiveCurrentQuarter == 0)
+            {
+                return -1;
+            }
+
+            if (bloodReceiveLastQuarter == 0)
+            {
+                return 1;
+            }
+
+            var percent = bloodReceiveCurrentQuarter / bloodReceiveLastQuarter - 1;
+
+            return (float) Math.Round(percent, 2);
+        }
+
+        public async Task<float> GetTotalBloodDonated()
+        {
+            var requests = await _requestRepository.GetRequestByStatus(1);
+            float total = requests.Sum(request => request.Quantity);
+            return total;
+        }
+
+        public async Task<float> GetBloodDonatedLastQuarter(long firstDayOfLastQuarter, long lastDayOfLastQuarter,
+            long firstDayOfCurrentQuarter)
+        {
+            var requests = await _requestRepository.GetRequestByStatus(1);
+            float bloodDonatedLastQuarter = 0;
+            float bloodDonatedCurrentQuarter = 0;
+
+            foreach (var request in requests)
+            {
+                var date = long.Parse(request.Date);
+                if (date >= firstDayOfLastQuarter && date <= lastDayOfLastQuarter)
+                {
+                    bloodDonatedLastQuarter += request.Quantity;
+                }
+
+                if (date >= firstDayOfCurrentQuarter)
+                {
+                    bloodDonatedCurrentQuarter += request.Quantity;
+                }
+            }
+
+            if (Math.Abs(bloodDonatedLastQuarter - bloodDonatedCurrentQuarter) == 0)
+            {
+                return 0;
+            }
+
+            if (bloodDonatedCurrentQuarter == 0)
+            {
+                return -1;
+            }
+
+            if (bloodDonatedLastQuarter == 0)
+            {
+                return 1;
+            }
+
+            var percent = bloodDonatedCurrentQuarter / bloodDonatedLastQuarter - 1;
+
+            return (float) Math.Round(percent, 2);
+        }
+
+        public async Task<List<string>> GetTotalDonators()
+        {
+            var listTransaction = new List<string>();
+            var transactions = await _donorTransactionRepository.GetTransactionByStatus(1);
+            foreach (var transaction in transactions)
+            {
+                if (!listTransaction.Contains(transaction.donorId))
+                {
+                    listTransaction.Add(transaction.donorId);
+                }
+            }
+
+            return listTransaction;
+        }
+
+        public async Task<int> GetDonatorsLastQuarter(long firstDayOfLastQuarter, long lastDayOfLastQuarter,
+            long firstDayOfCurrentQuarter)
+        {
+            var listDonatorsLastQuarter = new List<string>();
+            var listDonatorsCurrentQuarter = new List<string>();
+            var listDonor = new List<string>();
+            var transactions = await _donorTransactionRepository.GetTransactionByStatus(1);
+
+            var donorTransactions = transactions.ToList();
+            foreach (var transaction in donorTransactions)
+            {
+                var date = long.Parse(transaction.dateDonated);
+                if (date < firstDayOfLastQuarter && !listDonor.Contains(transaction.donorId))
+                {
+                    listDonor.Add(transaction.donorId);
+                }
+            }
+
+            foreach (var transaction in donorTransactions)
+            {
+                var date = long.Parse(transaction.dateDonated);
+                if (date >= firstDayOfLastQuarter && date <= lastDayOfLastQuarter &&
+                    !listDonatorsLastQuarter.Contains(transaction.donorId) && !listDonor.Contains(transaction.donorId))
+                {
+                    listDonatorsLastQuarter.Add(transaction.donorId);
+                }
+
+                if (date >= firstDayOfCurrentQuarter && !listDonatorsCurrentQuarter.Contains(transaction.donorId) &&
+                    !listDonatorsLastQuarter.Contains(transaction.donorId) && !listDonor.Contains(transaction.donorId))
+                {
+                    listDonatorsCurrentQuarter.Add(transaction.donorId);
+                }
+            }
+
+
+            return listDonatorsCurrentQuarter.Count - listDonatorsLastQuarter.Count;
+        }
+
+        public async Task<int> GetTotalEvents()
+        {
+            var events = await _eventRepository.Get();
+            return events.Count();
+        }
+
+        public async Task<int> GetEventsLastQuarter(long firstDayOfLastQuarter, long lastDayOfLastQuarter,
+            long firstDayOfCurrentQuarter, long lastDayOfCurrentQuarter)
+        {
+            var events = await _eventRepository.Get();
+            var listEvents = events.ToList();
+            var eventsLastQuarter = listEvents.Select(e => long.Parse(e.startDate))
+                .Count(date => date >= firstDayOfLastQuarter && date <= lastDayOfLastQuarter);
+            var eventsCurrentQuarter = listEvents.Select(e => long.Parse(e.startDate))
+                .Count(date => date >= firstDayOfCurrentQuarter && date <= lastDayOfCurrentQuarter);
+
+            return eventsCurrentQuarter - eventsLastQuarter;
         }
     }
 }
--- backend/Models/Dashboard.cs
+++ backend/Models/Dashboard.cs
@@ -5,12 +5,14 @@ namespace backend.Models
         public string _id { get; set; }
         public string date { get; set; }
         public string type { get; set; }
+        public string transactionId { get; set; }
 
-        public Id(string id, string date, string type)
+        public Id(string id, string date, string type, string transactionId)
         {
             _id = id;
             this.date = date;
             this.type = type;
+            this.transactionId = transactionId;
         }
     }
     
@@ -21,13 +23,79 @@ namespace backend.Models
         public string type { get; set; }
         public string detail { get; set; }
         public string date { get; set; }
+        public string transactionId { get; set; }
 
-        public RecentActivities(string id, string type, string detail, string date)
+        public RecentActivities(string id, string type, string detail, string date, string transactionId)
         {
             _id = id;
             this.type = type;
             this.detail = detail;
             this.date = date;
+            this.transactionId = transactionId;
+        }
+    }
+
+    public class BloodReceive
+    {
+        public float total { get; set; }
+        public float lastQuarter { get; set; }
+
+        public BloodReceive(float total, float lastQuarter)
+        {
+            this.total = total;
+            this.lastQuarter = lastQuarter;
+        }
+    }
+    
+    public class BloodDonated
+    {
+        public float total { get; set; }
+        public float lastQuarter { get; set; }
+
+        public BloodDonated(float total, float lastQuarter)
+        {
+            this.total = total;
+            this.lastQuarter = lastQuarter;
+        }
+    }
+
+    public class Donators
+    {
+        public int total { get; set; }
+        public int lastQuarter { get; set; }
+
+        public Donators(int total, int lastQuarter)
+        {
+            this.total = total;
+            this.lastQuarter = lastQuarter;
+        }
+    }
+    
+    public class Events
+    {
+        public int total { get; set; }
+        public int lastQuarter { get; set; }
+
+        public Events(int total, int lastQuarter)
+        {
+            this.total = total;
+            this.lastQuarter = lastQuarter;
+        }
+    }
+
+    public class DashboardInfo
+    {
+        public BloodReceive bloodReceive { get; set; }
+        public BloodDonated bloodDonated { get; set; }
+        public Donators donators { get; set; }
+        public Events events { get; set; }
+
+        public DashboardInfo(BloodReceive bloodReceive, BloodDonated bloodDonated, Donators donators, Events events)
+        {
+            this.bloodReceive = bloodReceive;
+            this.bloodDonated = bloodDonated;
+            this.donators = donators;
+            this.events = events;
         }
     }
 }
