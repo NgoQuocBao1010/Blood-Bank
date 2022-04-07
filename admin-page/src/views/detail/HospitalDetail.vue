fb6660988343f59567ffdac37d349cd06979bbee
lcnhut070200 <lcnhut070200@gmail.com>
2022-04-07 16:44:19 +0700

    refactor: change data

--- admin-page/src/views/detail/HospitalDetail.vue
+++ admin-page/src/views/detail/HospitalDetail.vue
@@ -3,14 +3,13 @@ import { defineAsyncComponent, onBeforeMount } from "vue";
 import Breadcrumb from "primevue/breadcrumb";
 
 import HospitalRepo from "../../api/HospitalRepo";
-import RequestRepo from "../../api/RequestRepo";
 
 const AsyncRequestHistory = defineAsyncComponent({
-    loader: () => import("../../components/tables/RequestHistoryTable.vue"),
+  loader: () => import("../../components/tables/RequestHistoryTable.vue"),
 });
 
 const { _id } = defineProps({
-    _id: String,
+  _id: String,
 });
 
 let requestHistory = $ref(null);
@@ -19,19 +18,15 @@ let showRequestHistory = $ref(false);
 
 // Naviagtion settings
 const home = $ref({
-    icon: "fa-solid fa-hospital",
-    to: { name: "Hospitals Management" },
+  icon: "fa-solid fa-hospital",
+  to: { name: "Hospitals Management" },
 });
 let items = $ref(null);
 
 onBeforeMount(async () => {
-    const hospitalData = await HospitalRepo.get(_id);
-    const requestData = await RequestRepo.getAll();
-
-    requestHistory = requestData.data;
-
-    console.log(requestHistory);
-    hospital = hospitalData.data;
+  const hospitalData = await HospitalRepo.get(_id);
+  hospital = hospitalData.data;
+  requestHistory = hospital.requestHistory;
 
   items = [
     {
@@ -42,107 +37,103 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-    <div class="grid">
-        <div class="col-12">
-            <!-- Navigation -->
-            <Breadcrumb
-                :home="home"
-                :model="items"
-                style="margin-bottom: 1rem; border-radius: 15px"
-            />
+  <div class="grid">
+    <div class="col-12">
+      <!-- Navigation -->
+      <Breadcrumb
+        :home="home"
+        :model="items"
+        style="margin-bottom: 1rem; border-radius: 15px"
+      />
 
-            <!-- Hospital Content -->
-            <div class="card">
-                <div class="card__content">
-                    <div class="card__header">
-                        <h2 class="card-title">
-                            {{ hospital && hospital.name }}
-                        </h2>
-                        <!-- Edit Button -->
-                        <RouterLink
-                            :to="{
-                                name: 'Hospital Edit',
-                                params: {
-                                    _id,
-                                    hospitalData: JSON.stringify(hospital),
-                                },
-                            }"
-                            v-ripple
-                            class="p-button p-button-sm p-component mb-2 p-ripple app-router-link-icon edit-btn"
-                        >
-                            <i class="fa-solid fa-pen-to-square"></i>
-                            Edit
-                        </RouterLink>
-                    </div>
+      <!-- Hospital Content -->
+      <div class="card">
+        <div class="card__content">
+          <div class="card__header">
+            <h2 class="card-title">
+              {{ hospital && hospital.name }}
+            </h2>
+            <!-- Edit Button -->
+            <RouterLink
+              :to="{
+                name: 'Hospital Edit',
+                params: {
+                  _id,
+                  hospitalData: JSON.stringify(hospital),
+                },
+              }"
+              v-ripple
+              class="p-button p-button-sm p-component mb-2 p-ripple app-router-link-icon edit-btn"
+            >
+              <i class="fa-solid fa-pen-to-square"></i>
+              Edit
+            </RouterLink>
+          </div>
 
-                    <!-- Details -->
-                    <div class="card__information">
-                        <div class="section">
-                            <p>
-                                <i class="fa-solid fa-passport"></i>
-                                {{ hospital && hospital._id }}
-                            </p>
-                            <p>
-                                <i class="fa-solid fa-location-pin"></i>
-                                {{ hospital && hospital.address }}
-                            </p>
-                            <p>
-                                <i class="fa-solid fa-phone"></i>
-                                {{ hospital && hospital.phone }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
+          <!-- Details -->
+          <div class="card__information">
+            <div class="section">
+              <p>
+                <i class="fa-solid fa-passport"></i>
+                {{ hospital && hospital._id }}
+              </p>
+              <p>
+                <i class="fa-solid fa-location-pin"></i>
+                {{ hospital && hospital.address }}
+              </p>
+              <p>
+                <i class="fa-solid fa-phone"></i>
+                {{ hospital && hospital.phone }}
+              </p>
             </div>
-
-            <!-- Request History -->
-            <div class="card">
-                <div
-                    class="flex-center"
-                    style="width: 100%"
-                    v-if="!showRequestHistory"
-                >
-                    <PrimeVueButton
-                        label="Show Request History"
-                        @click="showRequestHistory = !showRequestHistory"
-                    />
-                </div>
-
-                <template v-else>
-                    <h2>Request History</h2>
-                    <AsyncRequestHistory :requestHistory="requestHistory" />
-                </template>
-            </div>
+          </div>
         </div>
+      </div>
+
+      <!-- Request History -->
+      <div class="card">
+        <div class="flex-center" style="width: 100%" v-if="!showRequestHistory">
+          <PrimeVueButton
+            label="Show Request History"
+            @click="showRequestHistory = !showRequestHistory"
+          />
+        </div>
+
+        <template v-else>
+          <h2>Request History</h2>
+          <AsyncRequestHistory :requestHistory="requestHistory" />
+        </template>
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @import "../../assets/styles/badge.scss";
 .card {
-    &__content {
-        flex: 1 1 50%;
+  &__content {
+    flex: 1 1 50%;
 
-        p {
-            padding-left: 1rem;
-            i {
-                color: var(--primary-color);
-                font-size: 1.2rem;
-                padding-right: 1rem;
-            }
-        }
+    p {
+      padding-left: 1rem;
+      i {
+        color: var(--primary-color);
+        font-size: 1.2rem;
+        padding-right: 1rem;
+      }
     }
+  }
 
-    &__header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 1rem;
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 1rem;
 
-        .card-title {
-            color: var(--primary-color);
-            font-weight: 900;
-        }
+    .card-title {
+      color: var(--primary-color);
+      font-weight: 900;
     }
+  }
 }
 </style>
