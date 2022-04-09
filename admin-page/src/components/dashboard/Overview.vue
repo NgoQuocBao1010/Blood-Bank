2a54f35308d70c2f3b1f992bcbbdddeefb8b0479
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-04-09 18:32:32 +0700

    🆕 feat: overview skleton in draft

---
+++ admin-page/src/components/dashboard/Overview.vue
@@ -0,0 +1,105 @@
+<script setup>
+import { onBeforeMount } from "vue";
+
+const apiCall = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve({ name: "Quoc Bao" }), 2000);
+    });
+};
+
+let data = $ref(await apiCall());
+</script>
+
+<template>
+    <!-- Blood Receive -->
+    <div class="col-12 lg:col-6 xl:col-3">
+        <div class="card mb-0">
+            <div class="flex justify-content-between mb-3">
+                <div>
+                    <span class="block text-500 font-medium mb-3">
+                        Blood Receive
+                    </span>
+                    <div class="text-900 font-medium text-xl">25.00 L</div>
+                </div>
+                <div
+                    class="flex align-items-center justify-content-center bg-orange-100 border-round"
+                    style="width: 2.5rem; height: 2.5rem"
+                >
+                    <i
+                        class="text-orange-500 text-xl fa-solid fa-hand-holding-medical"
+                    ></i>
+                </div>
+            </div>
+            <span class="text-green-500 font-medium">%52+ </span>
+            <span class="text-500">since last quater</span>
+        </div>
+    </div>
+
+    <!-- Blood donated -->
+    <div class="col-12 lg:col-6 xl:col-3">
+        <div class="card mb-0">
+            <div class="flex justify-content-between mb-3">
+                <div>
+                    <span class="block text-400 font-medium mb-3"
+                        >Blood Donated</span
+                    >
+                    <div class="text-900 font-medium text-xl">500.00 L</div>
+                </div>
+                <div
+                    class="flex align-items-center justify-content-center bg-cyan-100 border-round"
+                    style="width: 2.5rem; height: 2.5rem"
+                >
+                    <i
+                        class="text-cyan-500 text-xl fa-solid fa-hand-holding-droplet"
+                    ></i>
+                </div>
+            </div>
+            <span class="text-500 font-medium text-decreased"> %17- </span>
+            <span class="text-500">since last quater</span>
+        </div>
+    </div>
+
+    <!-- Donators -->
+    <div class="col-12 lg:col-6 xl:col-3">
+        <div class="card mb-0">
+            <div class="flex justify-content-between mb-3">
+                <div>
+                    <span class="block text-500 font-medium mb-3"
+                        >Donators</span
+                    >
+                    <div class="text-900 font-medium text-xl">152</div>
+                </div>
+                <div
+                    class="flex align-items-center justify-content-center bg-blue-100 border-round"
+                    style="width: 2.5rem; height: 2.5rem"
+                >
+                    <i class="text-blue-500 text-xl fa-solid fa-users"></i>
+                </div>
+            </div>
+            <span class="text-green-500 font-medium">24 new </span>
+            <span class="text-500">since last quater</span>
+        </div>
+    </div>
+
+    <!-- Events -->
+    <div class="col-12 lg:col-6 xl:col-3">
+        <div class="card mb-0">
+            <div class="flex justify-content-between mb-3">
+                <div>
+                    <span class="block text-500 font-medium mb-3">Events</span>
+                    <div class="text-900 font-medium text-xl">30</div>
+                </div>
+                <div
+                    class="flex align-items-center justify-content-center bg-purple-100 border-round"
+                    style="width: 2.5rem; height: 2.5rem"
+                >
+                    <i class="pi pi-calendar text-purple-500 text-xl"></i>
+                </div>
+            </div>
+            <span class="text-green-500 font-medium">5 new </span>
+            <span class="text-500">since last quater</span>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
--- admin-page/src/views/Dashboard.vue
+++ admin-page/src/views/Dashboard.vue
@@ -2,6 +2,8 @@
 import Chart from "primevue/chart";
 import Menu from "primevue/menu";
 
+import Overview from "../components/dashboard/Overview.vue";
+import OverviewSkeleton from "../components/dashboard/OverviewSkeleton.vue";
 import RecentActivities from "../components/dashboard/Recent.vue";
 import { THEMES } from "../constants/index.js";
 
@@ -50,97 +52,14 @@ let menuItems = $ref([
 <template>
     <div class="grid">
         <!-- General Information -->
-        <!-- Blood Receive -->
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-500 font-medium mb-3">
-                            Blood Receive
-                        </span>
-                        <div class="text-900 font-medium text-xl">25.00 L</div>
-                    </div>
-                    <div
-                        class="flex align-items-center justify-content-center bg-orange-100 border-round"
-                        style="width: 2.5rem; height: 2.5rem"
-                    >
-                        <i
-                            class="text-orange-500 text-xl fa-solid fa-hand-holding-medical"
-                        ></i>
-                    </div>
-                </div>
-                <span class="text-green-500 font-medium">%52+ </span>
-                <span class="text-500">since last quater</span>
-            </div>
-        </div>
-
-        <!-- Blood donated -->
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-400 font-medium mb-3"
-                            >Blood Donated</span
-                        >
-                        <div class="text-900 font-medium text-xl">500.00 L</div>
-                    </div>
-                    <div
-                        class="flex align-items-center justify-content-center bg-cyan-100 border-round"
-                        style="width: 2.5rem; height: 2.5rem"
-                    >
-                        <i
-                            class="text-cyan-500 text-xl fa-solid fa-hand-holding-droplet"
-                        ></i>
-                    </div>
-                </div>
-                <span class="text-500 font-medium text-decreased"> %17- </span>
-                <span class="text-500">since last quater</span>
-            </div>
-        </div>
-
-        <!-- Donators -->
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-500 font-medium mb-3"
-                            >Donators</span
-                        >
-                        <div class="text-900 font-medium text-xl">152</div>
-                    </div>
-                    <div
-                        class="flex align-items-center justify-content-center bg-blue-100 border-round"
-                        style="width: 2.5rem; height: 2.5rem"
-                    >
-                        <i class="text-blue-500 text-xl fa-solid fa-users"></i>
-                    </div>
-                </div>
-                <span class="text-green-500 font-medium">24 new </span>
-                <span class="text-500">since last quater</span>
-            </div>
-        </div>
-
-        <!-- Events -->
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-500 font-medium mb-3"
-                            >Events</span
-                        >
-                        <div class="text-900 font-medium text-xl">30</div>
-                    </div>
-                    <div
-                        class="flex align-items-center justify-content-center bg-purple-100 border-round"
-                        style="width: 2.5rem; height: 2.5rem"
-                    >
-                        <i class="pi pi-calendar text-purple-500 text-xl"></i>
-                    </div>
-                </div>
-                <span class="text-green-500 font-medium">5 new </span>
-                <span class="text-500">since last quater</span>
-            </div>
-        </div>
+        <Suspense>
+            <template #default>
+                <Overview />
+            </template>
+            <template #fallback>
+                <OverviewSkeleton />
+            </template>
+        </Suspense>
 
         <!-- Recent Activities && Blood Type Storage -->
         <div class="col-12 xl:col-6">
