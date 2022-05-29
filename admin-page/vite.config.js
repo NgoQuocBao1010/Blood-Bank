6155face744436566e7768a55b360c6dbe812799
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-05-29 16:34:12 +0700

    👨‍💻 updated: new build script

--- admin-page/vite.config.js
+++ admin-page/vite.config.js
@@ -5,8 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    baseUrl: "/admin/",
-    base: "./",
     plugins: [
         vue({
             script: {
